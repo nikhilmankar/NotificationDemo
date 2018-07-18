a099526a475ce4913851888ef6872adc9638e2a3
nikhilmankar <nikhil.mankar7171@gmail.com>
2018-07-18 16:10:48 +0530

    First Commit

---
+++ app/src/main/java/com/example/nikhil/notificationdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.nikhil.notificationdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Token", "Refreshed token: " + refreshedToken);
+    }
+}
